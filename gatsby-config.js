53c50665a84f68c23baa3bfc258297088066f30c
Robert Witkowski <robert.witkowski@altkomsoftware.pl>
2019-08-06 20:04:34 +0200

    Better description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `AfterFit`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `TODO`,
     author: `WPieniak`,
   },
   plugins: [
