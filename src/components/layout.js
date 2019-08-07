a2c796183a3612b2b6db480a6cf942fbe656d663
Waldek <waldek.pieniak@gmail.com>
2019-08-07 09:48:34 +0200

    Navigate to cookie city when on main page

--- src/components/layout.js
+++ src/components/layout.js
@@ -15,7 +15,7 @@ const Layout = ({title, children, pageContext }) => {
   const citySlug = pageContext && pageContext.city
   const dietSlug = pageContext && pageContext.diet
   // redirect from main page when the cookie is set
-  // useEffect(() => !citySlug && cityCookie && cityCookie !== 'lodz' && navigate(dietSlug ? `/${cityCookie}/${dietSlug}` :`/${cityCookie}`), [])
+  useEffect(() => !citySlug && cityCookie && cityCookie !== 'lodz' && navigate(dietSlug ? `/${cityCookie}/${dietSlug}` :`/${cityCookie}`), [])
   useEffect(() => {
       citySlug && typeof window !== 'undefined' && window.sessionStorage.setItem('city', JSON.stringify(citySlug))
   }, [])
