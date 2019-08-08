1c011f4035d61e9211f543e333a4d45b702acefc
Waldek <waldek.pieniak@gmail.com>
2019-08-08 19:36:09 +0200

    Value proposition texts implemented in Bydgoszcz and Krakow

--- src/data/mainPage/valueProposition.js
+++ src/data/mainPage/valueProposition.js
@@ -9,4 +9,14 @@ export const valuePropositionData = {
     variety: "Proponowane przez nas menu jest niezwykle urozmaicone. Szukamy inspiracji w kuchniach z całego świata, stąd tak szeroki wybór. Stawiamy na oryginalność, odpowiedni smak i różnorodność. Od teraz, nie będziesz narzekać na nudę.",
     quality: "Oferujemy najwyższą jakość. Do przygotowania każdego posiłku, używamy wyłącznie najlepszych, świeżych i sprawdzonych produktów. Nasze diety zostały stworzone z pełnym zaangażowaniem oraz pasją.",
   },
+  bydgoszcz: {
+    elasticity: "AfterFit zawsze dostosowuje się do Klientów. Nasza dieta pudełkowa jest niezwykle elastyczna – Ty sam możesz dowolnie komponować swoje menu. Nie narzucamy sztywnego jadłospisu. Wszystko dopasowujesz pod własne preferencje. Możesz zdecydować się tylko na te dania, które najbardziej Ci smakują.",
+    variety: "Oferujemy urozmaicone diety pudełkowe. Nasze menu jest oryginalne, smaczne i przede wszystkim różnorodne. Korzystając z AfterFit nigdy nie będziesz znudzony posiłkami. Inspirujemy się smakami z całego świata, a nasze pomysły wcielamy w codzienne dania.",
+    quality: "Dieta pudełkowa to posiłki smaczne i zdrowe. Żeby utrzymać najwyższy poziom, niezbędne są składniki odpowiedniej jakości. Każde danie z oferty AfterFit jest przygotowywane ze świeżych i sprawdzonych składników. Menu układamy z zaangażowaniem oraz pasją, starając się zaspokajać oczekiwania naszych Klientów.",
+  },
+  krakow: {
+    elasticity: "W cateringu dietetycznym AfterFit dowolnie komponujesz swoje menu. Możesz wybierać tylko te dania, które najbardziej Ci zasmakowały. Już teraz dostosuj wszystko pod swoje preferencje.",
+    variety: "Dieta pudełkowa z AfterFit to gwarancja różnorodności. Proponujemy oryginalne, smaczne dania, do których inspiracji szukamy w kuchniach całego świata. Nasze jedzenie nie jest nudne! Każdego dnia możesz próbować nowych posiłków!",
+    quality: "Odpowiednia jakość to nasz priorytet. Każde danie jest przygotowywane z najlepszych, świeżych składników, pochodzących z pewnych i sprawdzonych źródeł.",
+  },
 }
