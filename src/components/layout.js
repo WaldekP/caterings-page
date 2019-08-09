8c9ed53cf6748d9806a7ca0dea000442a493e928
Waldek <waldek.pieniak@gmail.com>
2019-08-09 12:29:04 +0200

    Overlay last try

--- src/components/layout.js
+++ src/components/layout.js
@@ -117,7 +117,7 @@ const Layout = ({title, children, pageContext }) => {
       {/*      </ul>*/}
       {/*    </div>*/}
       {/*  </div>}*/}
-      <div className={layoutStyles.overlay} style={(typeof overlay !== 'undefined' && !overlay) || citySlug ? {display: 'none'} : null}>
+      <div className={layoutStyles.overlay} style={(typeof overlay !== 'undefined' && !overlay) || citySlug || cityCookie2 ? {display: 'none'} : null}>
         {console.log('overlay', overlay)}
         <div className={layoutStyles.overlayContent}>
           <h2>Wybierz miasto:</h2>
