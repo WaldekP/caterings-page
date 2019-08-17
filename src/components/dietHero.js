6008615e7d80aeffe7f2a0a9247db587a5f35561
Waldek <waldek.pieniak@gmail.com>
2019-08-17 14:22:52 +0200

    Styles to paleo diet added

--- src/components/dietHero.js
+++ src/components/dietHero.js
@@ -2,9 +2,19 @@ import React from "react"
 import heroStyles from "../styles/dietHero.module.scss"
 import bac from "../images/diets/dietPageMainImages/Dieta odchudzająca catering dietetyczny AfterFit catering.jpg"
 
-const DietHero = () => {
+const DietHero = ({diet}) => {
+  const getDietClass = () => {
+    if (diet === 'dieta-odchudzajaca') {
+      return heroStyles.dietWeightLoss
+    }
+    if (diet === 'dieta-paleo') {
+      return heroStyles.dietPaleo
+    }
+  }
+
+
   return (
-      <div className={`${heroStyles.container} ${heroStyles.dietWeightLoss}`} />
+      <div className={`${heroStyles.container} ${getDietClass()}`} />
   )
 }
 
--- src/pages/diet.js
+++ src/pages/diet.js
@@ -5,21 +5,22 @@ import DietDescription from "../components/dietDescription"
 import { diets } from "../data/diets"
 import { cities } from "../data/cities"
 import DietGallery from "../components/dietGallery"
-import DietsFaq from '../components/dietsFaq'
-import DietPricing from '../components/dietPricing'
-import CallToAction from '../components/callToAction'
+import DietsFaq from "../components/dietsFaq"
+import DietPricing from "../components/dietPricing"
+import CallToAction from "../components/callToAction"
 
-
-const Diet = ({pageContext}) => {
-
+const Diet = ({ pageContext }) => {
   const findDiet = () => {
     const dietSlug = pageContext.diet
     return diets.find(diet => diet.dietUrl === dietSlug)
   }
 
   const getCitySlug = () => {
-    return pageContext.city ? pageContext.city : 'lodz'
+    return pageContext.city ? pageContext.city : "lodz"
+  }
 
+  const getDietSlug = () => {
+    return pageContext && pageContext.diet
   }
 
   const getDietAltTag = () => {
@@ -33,7 +34,7 @@ const Diet = ({pageContext}) => {
   }
   const findCity = () => {
     const citySlug = getCitySlug()
-    const cityObject =  cities.find(city => city.value === citySlug)
+    const cityObject = cities.find(city => city.value === citySlug)
     return cityObject && cityObject.label
   }
 
@@ -43,21 +44,41 @@ const Diet = ({pageContext}) => {
     if (!(diet && city)) {
       return null
     }
-    const descriptions = diet && diet.cityDescription[city] ? diet.cityDescription[city] : diet.cityDescription['lodz']
+    const descriptions =
+      diet && diet.cityDescription[city]
+        ? diet.cityDescription[city]
+        : diet.cityDescription["lodz"]
     return descriptions && descriptions.description
   }
 
   return (
     <Layout pageContext={pageContext}>
-      <DietHero />
-      <DietDescription diet={findDiet() && findDiet().fullName} city={findCity()} dietDescription={findDietDescription()} menu={findDiet() && findDiet().menu}/>
-      <DietGallery getDietAltTag={getDietAltTag} dietGallery={findDiet() && findDiet().dietGallery}/>
-      <DietsFaq pageContext={pageContext} dietQuestions={findDiet() &&  findDiet().questions && Object.values(findDiet().questions)}/>
-      <DietPricing pageContext={pageContext} diet={findDiet() && findDiet().fullName}/>
+      <DietHero diet={pageContext && getDietSlug()}/>
+      <DietDescription
+        diet={findDiet() && findDiet().fullName}
+        city={findCity()}
+        dietDescription={findDietDescription()}
+        menu={findDiet() && findDiet().menu}
+      />
+      <DietGallery
+        getDietAltTag={getDietAltTag}
+        dietGallery={findDiet() && findDiet().dietGallery}
+      />
+      <DietsFaq
+        pageContext={pageContext}
+        dietQuestions={
+          findDiet() &&
+          findDiet().questions &&
+          Object.values(findDiet().questions)
+        }
+      />
+      <DietPricing
+        pageContext={pageContext}
+        diet={findDiet() && findDiet().fullName}
+      />
       <CallToAction cta="diet" />
     </Layout>
   )
 }
-
 
 export default Diet
