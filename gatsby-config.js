0e8c518a02a12187309f54d740e17cb79e6b2a14
Waldek <waldek.pieniak@gmail.com>
2019-08-17 22:40:41 +0200

    Google tag manager added

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,6 +42,23 @@ module.exports = {
         }
       }
     },
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-5N36QQ7",
+
+        // Include GTM in development.
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" },
+
+        // Specify optional GTM environment details.
+      },
+    },
     // {
     //   resolve: `gatsby-source-wordpress`,
     //   options: {
