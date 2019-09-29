bed6eca6a669e325e2d6b4a7914169542965283b
Waldek <waldek.pieniak@gmail.com>
2019-09-29 19:26:26 +0200

    Service worker added

--- gatsby-config.js
+++ gatsby-config.js
@@ -109,12 +109,13 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#FBDAE2`,
+        theme_color: `#FBDAE2`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-offline`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
